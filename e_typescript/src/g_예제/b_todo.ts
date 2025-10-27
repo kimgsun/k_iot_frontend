1778363dd9de6a52055bf32c7bba259fbebc45b1
kimgsun <gsune@kakao.com>
2025-10-27 16:10:51 +0900

    20251027 TypeScript

--- e_typescript/src/g_예제/b_todo.ts
+++ e_typescript/src/g_예제/b_todo.ts
@@ -1,6 +1,6 @@
 // b_todo.ts
 
-//! 타입스크립트 TODO 리스트 구현 
+//! 타입스크립트 TODO 리스트 구현
 
 /*
 @ 데이터 구조
@@ -39,10 +39,68 @@ interface TodoItem {
 // 6) 모든 Todo 항목의 completed 상태를 일괄적으로 설정하는 함수(setAllTodosCompletion)
 
 //^ 3. 프로그램 구현
-function editTodo() {
 
+// 1) 특정 id를 가진 Todo 항목의 task를 편집하는 함수(editTodo)
+function editTodo(todos: TodoItem[], id: number, newTask: string): TodoItem[] {
+  return todos.map((todo) =>
+    todo.id === id ? { ...todo, task: newTask } : todo
+  );
 }
 
+// 2) 완료된 Todo 항목을 모두 삭제하는 함수(clearCompleted)
+function clearCompleted(todos: TodoItem[]): TodoItem[] {
+  return todos.filter((todo) => !todo.completed);
+}
+
+// 3) 모든 Todo 항목을 조회하는 함수(getAllTodos)
+function getAllTodos(todos: TodoItem[]): TodoItem[] {
+  return todos;
+}
+
+// 4) 특정 상태(completed)에 따라 Todo 항목을 필터링하는 함수(filterTodos)
+function filterTodos(todos: TodoItem[], completed: boolean): TodoItem[] {
+  return todos.filter((todo) => todo.completed === completed);
+}
+
+// 5) 특정 id를 가진 Todo 항목의 completed 상태를 토글하는 함수(toggleTodo)
+function toggleTodo(todos: TodoItem[], id: number): TodoItem[] {
+  return todos.map((todo) =>
+    todo.id === id ? { ...todo, completed: !todo.completed } : todo
+  );
+}
+
+// 6) 모든 Todo 항목의 completed 상태를 일괄적으로 설정하는 함수(setAllTodosCompletion)
+function setAllTodosCompletion(
+  todos: TodoItem[],
+  completed: boolean
+): TodoItem[] {
+  return todos.map((todo) => ({ ...todo, completed }));
+}
 
 //^ 4. 프로그램 실행
-let todos: TodoItem[] = [];
+let todos: TodoItem[] = [
+  { id: 1, task: "abc", completed: false },
+  { id: 2, task: "def", completed: true },
+  { id: 3, task: "ghi", completed: false },
+  { id: 4, task: "jkl", completed: false },
+  { id: 5, task: "mno", completed: true },
+  { id: 6, task: "pqr", completed: false },
+  { id: 7, task: "stu", completed: true },
+  { id: 8, task: "vwx", completed: true },
+  { id: 9, task: "yz", completed: false },
+];
+
+todos = editTodo(todos, 2, "자바스크립트");
+console.log(todos);
+
+todos = filterTodos(todos, false);
+console.log(todos);
+
+todos = clearCompleted(todos);
+console.log(todos);
+
+todos = toggleTodo(todos, 3);
+console.log(todos);
+
+todos = setAllTodosCompletion(todos, false);
+console.log(getAllTodos(todos));
