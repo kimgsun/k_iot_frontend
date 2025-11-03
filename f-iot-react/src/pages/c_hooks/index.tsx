91bb4c6784ba18dd87f56df6be86f3fad8d3804e
kimgsun <gsune@kakao.com>
2025-11-03 17:46:05 +0900

    20251103 React Hooks

--- f-iot-react/src/pages/c_hooks/index.tsx
+++ f-iot-react/src/pages/c_hooks/index.tsx
@@ -1,36 +1,59 @@
-import React from 'react'
-import State01 from './a_useState/State01'
-import State02 from './a_useState/State02'
-import State03 from './a_useState/State03'
-import State04 from './a_useState/State04'
-import State05 from './a_useState/State05'
-import State06 from './a_useState/State06'
-import State06_Child from './a_useState/State06_Child'
-import Ref01 from './b_useRef/Ref01'
+import React, { useState } from "react";
+import State01 from "./a_useState/State01";
+import State02 from "./a_useState/State02";
+import State03 from "./a_useState/State03";
+import State04 from "./a_useState/State04";
+import State05 from "./a_useState/State05";
+import State06 from "./a_useState/State06";
+import Ref01 from "./b_useRef/Ref01";
+import CollapsibleSection from "@/components/CollapsibleSection";
 
 const h2Style = {
-  backgroundColor: 'black',
-  color: 'orange'
-}
+  backgroundColor: "black",
+  color: "orange",
+  padding: "8px",
+  cursor: "pointer",
+};
 
+// ✅ Index 컴포넌트
 function Index() {
+  // 각 섹션의 열림 상태를 배열로 관리
+  const [sections, setSections] = useState([true, false]);
+
+  const toggleSection = (index: number) => {
+    setSections((prev) =>
+      prev.map((isOpen, i) => (i === index ? !isOpen : isOpen))
+    );
+  };
+
   return (
     <div>
-      <h1 style={{ backgroundColor: 'black', color: 'white' }}>
+      <h1 style={{ backgroundColor: "black", color: "white", padding: "8px" }}>
         === 리액트 Hooks ===
       </h1>
-      <h2 style={h2Style}>2. 리액트 Hooks - useRef</h2>
-      <Ref01 />
 
-      <h2 style={h2Style}>1. 리액트 Hooks - useState</h2>
-      <State01 /> <hr />
-      <State02 /> <hr />
-      <State03 /> <hr />
-      <State04 /> <hr />
-      <State05 /> <hr />
-      <State06 /> <hr />
+      <CollapsibleSection
+        title="1. 리액트 Hooks - useState"
+        isOpen={sections[0]}
+        onToggle={() => toggleSection(0)}
+      >
+        <State01 /> <hr />
+        <State02 /> <hr />
+        <State03 /> <hr />
+        <State04 /> <hr />
+        <State05 /> <hr />
+        <State06 /> <hr />
+      </CollapsibleSection>
+
+      <CollapsibleSection
+        title="2. 리액트 Hooks - useRef"
+        isOpen={sections[1]}
+        onToggle={() => toggleSection(1)}
+      >
+        <Ref01 />
+      </CollapsibleSection>
     </div>
-  )
+  );
 }
 
-export default Index
+export default Index;
